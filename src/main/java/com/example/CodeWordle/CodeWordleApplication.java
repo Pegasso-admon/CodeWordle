57103508ee83f4eae2bc51215bd92f7c0bb24540
Samuel Rosero Alvarez <p3g455o48@gmail.com>
2025-10-31 17:27:30 -0500

    first commit

---
+++ src/main/java/com/example/CodeWordle/CodeWordleApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeWordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWordleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeWordleApplication.class, args);
+	}
+
+}
